72064eed67e430761692992b76427819b428fb53
prashantgamre <prashant.gamre@vauchinfotech.com>
2025-09-12 00:10:35 +0530

    Update agent.py
    updated the google llm

--- src/agent.py
+++ src/agent.py
@@ -1,24 +1,27 @@
 from dotenv import load_dotenv
-
 from livekit import agents
-from livekit.agents import AgentSession, Agent, RoomInputOptions
-from livekit.plugins import (noise_cancellation)
-from livekit.plugins import openai
+from livekit.agents import AgentSession, Agent, RoomInputOptions , function_tool
+from livekit.plugins import noise_cancellation
+from livekit.plugins import google
+from prompts import Assistant_instructions
+import os
 
 load_dotenv(".env.local")
+
+api_key=os.getenv("Google_API_KEY")
+
 
 
 class Assistant(Agent):
     def __init__(self) -> None:
-        super().__init__(instructions="You are a helpful voice AI assistant.")
+        super().__init__(instructions=Assistant_instructions)
+
 
 
 async def entrypoint(ctx: agents.JobContext):
     session = AgentSession(
-        llm=openai.realtime.RealtimeModel(
-            voice="Charon"
+        llm=google.beta.realtime.RealtimeModel(model="gemini-2.0-flash-exp")
         )
-    )
 
     await session.start(
         room=ctx.room,
@@ -26,13 +29,15 @@ async def entrypoint(ctx: agents.JobContext):
         room_input_options=RoomInputOptions(
             # For telephony applications, use `BVCTelephony` instead for best results
             noise_cancellation=noise_cancellation.BVC(),
-        ),
+        )
     )
 
     await session.generate_reply(
-        instructions="Greet the user in hindi and aks wchich language user want to chat with you."
+        instructions="Greet the user and tell i am Aeon Credit Service AI Voice Assistant in hindi and aks wchich language user want to chat with you."
     )
 
 
 if __name__ == "__main__":
     agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
+    
+
